9dc44d9f93b3b62ded122b31114c48b152a9a7e7
voodik <voodik.am@gmail.com>
2020-11-20 23:47:03 +0800

    ODROID: remove persist.pwbtn.shutdown, handle it via Settings
    Change-Id: Id3bd43d4cd8467868dd9b0042634643e33c2580f

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -530,8 +530,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private int mDeviceHardwareKeys;
     private boolean mHandleVolumeKeysInWM;
 
-    boolean mPwbtnForceShutdownprop;
-
     // Button wake control flags
     boolean mWakeOnHomeKeyPress;
     boolean mWakeOnMenuKeyPress;
@@ -1426,9 +1424,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 performHapticFeedback(HapticFeedbackConstants.LONG_PRESS, false,
                         "Power - Long Press - Global Actions");
                 showGlobalActionsInternal();
-                if (mPwbtnForceShutdownprop){
-                    mLongPressOnPowerBehavior = LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM;
-                }
                 break;
             case LONG_PRESS_POWER_SHUT_OFF:
             case LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM:
@@ -2099,7 +2094,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mPowerKeyWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                 "PhoneWindowManager.mPowerKeyWakeLock");
         mEnableShiftMenuBugReports = "1".equals(SystemProperties.get("ro.debuggable"));
-        mPwbtnForceShutdownprop = SystemProperties.getBoolean("persist.pwbtn.shutdown", false);
         mLidKeyboardAccessibility = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_lidKeyboardAccessibility);
         mLidNavigationAccessibility = mContext.getResources().getInteger(
@@ -2134,13 +2128,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         mShortPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_shortPressOnPowerBehavior);
-        if (mPwbtnForceShutdownprop){
-        mLongPressOnPowerBehavior = LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM;
-		Slog.i(TAG, "MNG mPwbtnForceShutdownprop");
-        } else {
         mLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_longPressOnPowerBehavior);
-        }
         mVeryLongPressOnPowerBehavior = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_veryLongPressOnPowerBehavior);
         mDoublePressOnPowerBehavior = mContext.getResources().getInteger(
