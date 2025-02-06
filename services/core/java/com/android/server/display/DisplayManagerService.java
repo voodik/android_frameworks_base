5e3e7626a312ef6caf62eff6a2a36f8376c2353f
voodik <voodik.am@gmail.com>
2025-02-06 14:39:09 +0400

    ODROID: Wifi display only when FEATURE_WIFI_DIRECT enabled
    Change-Id: Id4f45037b8da8d70d42050cc3c83fb62132d905f

--- services/core/java/com/android/server/display/DisplayManagerService.java
+++ services/core/java/com/android/server/display/DisplayManagerService.java
@@ -2232,9 +2232,10 @@ public final class DisplayManagerService extends SystemService {
     }
 
     private void registerWifiDisplayAdapterLocked() {
-        if (mContext.getResources().getBoolean(
+        if ((mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_enableWifiDisplay)
-                || SystemProperties.getInt(FORCE_WIFI_DISPLAY_ENABLE, -1) == 1) {
+                || SystemProperties.getInt(FORCE_WIFI_DISPLAY_ENABLE, -1) == 1)
+                && mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_DIRECT)) {
             mWifiDisplayAdapter = new WifiDisplayAdapter(
                     mSyncRoot, mContext, mHandler, mDisplayDeviceRepo,
                     mPersistentDataStore, mFlags);
