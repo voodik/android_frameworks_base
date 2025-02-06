e0437ae38300b9bf18b77cadf6a5209f331402f6
voodik <voodik.am@gmail.com>
2025-02-06 14:36:42 +0400

    ODROID: enable features by prop
    Change-Id: I4bc39951531353c1677172d2521d42e5c31435af

--- services/core/java/com/android/server/SystemConfig.java
+++ services/core/java/com/android/server/SystemConfig.java
@@ -1041,6 +1041,19 @@ public class SystemConfig {
                                 String notLowRam = parser.getAttributeValue(null, "notLowRam");
                                 allowed = !"true".equals(notLowRam);
                             }
+                            if (fname.contains("android.hardware.hdmi.cec")) {
+                                allowed = SystemProperties.getBoolean("persist.hdmi_cec.enable", false);
+                                Slog.w(TAG, "MNG " + fname + " allowed=" + allowed);
+                            }
+                            if (fname.contains("android.hardware.bluetooth")) {
+                                allowed = !SystemProperties.getBoolean("persist.disable_bluetooth", true);
+                                Slog.w(TAG, "MNG " + fname + " allowed=" + allowed);
+                            }
+                            if (fname.contains("android.hardware.wifi.direct")) {
+                                allowed = SystemProperties.get("wifi.config.prefix", "wifi")
+                                        .toLowerCase().contains("p2p");
+                                Slog.w(TAG, "MNG " + fname + " allowed=" + allowed);
+                            }
                             if (fname == null) {
                                 Slog.w(TAG, "<" + name + "> without name in " + permFile + " at "
                                         + parser.getPositionDescription());
