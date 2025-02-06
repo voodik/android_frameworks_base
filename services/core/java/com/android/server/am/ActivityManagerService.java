ded502ea5870a90bba751246f8439a734b025488
voodik <voodik.am@gmail.com>
2025-02-06 14:37:50 +0400

    ODROID: disable console notification
    Change-Id: Ie2cddbe3f7d8ed18638ed0e9c027e5e9823777ea

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5196,7 +5196,7 @@ public class ActivityManagerService extends IActivityManager.Stub
             mUserController.scheduleStartProfiles();
         }
         // UART is on if init's console service is running, send a warning notification.
-        showConsoleNotificationIfActive();
+//        showConsoleNotificationIfActive();
         showMteOverrideNotificationIfActive();
 
         t.traceEnd();
